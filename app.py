32fbfe3b45ba11a01085c348743a29ca27e3fbb4
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2023-11-06 05:47:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,74 @@
-import streamlit as st
-import langchain
-import time
+mport streamlit as st
 from langchain.llms import GooglePalm
-from langchain.chains import RetrievalQA
-from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import SequentialChain
+import time
 
 
-llm = GooglePalm(google_api_key=st.secrets["api_key"], temperature=0.5, max_tokens=300)
 
+llm = GooglePalm(google_api_key=st.secrets["api_key"], temperature=0.5)
 
-st.write("This is :red[test]")
 st.write(':red[If this page returns an error, it is probably because the Google generative ai API key is unavailable where the app is running.]')
-st.title('QA Chatbot')
+st.title('Vacation City and Sightseeing Recommender')
 st.image('cover.jpg')
-st.subheader('Please enter your question here about our courses:.')
+st.subheader('First, select which month and continent you wanna travel,\
+             then click on the Recommend button, to see the city and places to visit!')
 
-with st.spinner('Loading, please wait...'):
-  loader = CSVLoader(file_path='QA.csv', source_column= 'prompt',encoding='cp1252')
-  data = loader.load()
-  embeddings = GooglePalmEmbeddings(google_api_key=st.secrets["api_key"])
-  vectorindex_googlepalm = FAISS.from_documents(data, embeddings)
-  retriever = vectorindex_googlepalm.as_retriever()
-  vectorindex_googlepalm.save_local('vectordatabase')
+col1, col2 = st.columns(2)
 
-question = st.text_input('enter your question here:')
+with col1:
+    month = st.selectbox(label='##### select the month', options=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+with col2:
+    continent = st.selectbox(label='##### select the continent', options=['Asia','Europe','Africa','North America','South America','Oceania'])
 
-if question: 
-    vectorindex_googlepalm = FAISS.load_local('vectordatabase',embeddings)
 
-    retriever = vectorindex_googlepalm.as_retriever(score_threshold=0.7)
+col1, col2, col3 , col4, col5 = st.columns(5)
 
-    chain = RetrievalQA.from_chain_type(llm= llm,
-                              chain_type='stuff',
-                              retriever = retriever,
-                              input_key='query',
-                              return_source_documents=True)
+with col1:
+    pass
+with col2:
+    pass
+with col4:
+    pass
+with col5:
+    pass
+with col3 :
+    button = st.button('Recommend!')
 
-    answer = chain(question)
 
-    st.header('BOT: ')
-    st.write(answer['result'])
+def main():
+
+    prompt_template_city = PromptTemplate(
+    input_variables=['month','continent'],
+    template = 'Please suggest to me a city to go for a vacation in {continent} in {month}, please justify your suggestion, why you tell me to visit this city in {month}?'
+    )   
+
+    city_chain = LLMChain(llm = llm, 
+                      prompt = prompt_template_city,
+                      output_key='city')
+
+
+    prompt_template_sight = PromptTemplate(
+        input_variables=['city'],
+        template = 'please tell me the three major sightseeings of {city} city and their address in the {city} and make them bold and in bulletpoint'
+    )
+
+    sight_chain = LLMChain(llm = llm,
+                        prompt = prompt_template_sight,
+                        output_key='major sightseeings')
+
+    chain = SequentialChain(chains = [city_chain,sight_chain],
+                        input_variables=['month','continent'],
+                        output_variables=['city','major sightseeings'])
+
+    answer = chain({'month':month,'continent':continent})
+
+    if button:
+        with st.spinner('Please wait a second...!'):
+          time.sleep(8)
+        st.subheader(f'The most recommended city to visit in {answer["continent"]} in {answer["month"]} is {answer["city"]}')     
+
+
+if __name__ == '__main__':
+    main()
